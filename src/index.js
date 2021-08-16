8cfb037192d7e75b1bfe025ad5bd59d6b127924e
Yulia Lepner <lepneryulia@gmail.com>
2021-08-16 17:30:20 +0200

    Completed task

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
+let units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let upTo20 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let dozens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
 module.exports = function toReadable(number) {
-  let units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  let upTo20 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  let dozens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-  let bigNumber = ['hundred', 'thousand'];
 
   myNumberArray = Array.from(String(number), Number)
   if (myNumberArray.length === 1) {
     return units[myNumberArray[0]]
   }
   if (myNumberArray.length === 2) {
-    if (myNumberArray[0] === 1) {
-      return upTo20[myNumberArray[1]]
-    }
-    if (myNumberArray[1] === 0) {
-      return answer = `${dozens[myNumberArray[0]]}`
-    } else {
-      return answer = `${dozens[myNumberArray[0]]} ${units[myNumberArray[1]]}`
-    }
+    return twoDigitToHumanReadable(myNumberArray[0], myNumberArray[1])
   }
   if (myNumberArray.length === 3) {
-    if (myNumberArray[1] === 0) {
-      if (myNumberArray[2] === 0) {
-        return answer = `${units[myNumberArray[0]]} ${bigNumber[0]}`
-      }
-      return answer = `${units[myNumberArray[0]]} ${bigNumber[0]} ${units[myNumberArray[2]]}`
+    if (myNumberArray[1] === 0 && myNumberArray[2] === 0) {
+      return `${units[myNumberArray[0]]} hundred`
     }
-    if (myNumberArray[1] === 1) {
-      return answer = `${units[myNumberArray[0]]} ${bigNumber[0]} ${upTo20[myNumberArray[2]]}`
-    }
-    if (myNumberArray[2] === 0) {
-      return answer = `${units[myNumberArray[0]]} ${bigNumber[0]} ${dozens[myNumberArray[1]]}`
-    } else {
-      return answer = `${units[myNumberArray[0]]} ${bigNumber[0]} ${dozens[myNumberArray[1]]} ${units[myNumberArray[2]]}`
-    }
+    return `${units[myNumberArray[0]]} hundred ${twoDigitToHumanReadable(myNumberArray[1], myNumberArray[2])}`
   }
+}
+
+function twoDigitToHumanReadable(dozen, unit) {
+  if (dozen === 1) {
+    return upTo20[unit]
+  }
+  if (unit === 0) {
+    return `${dozens[dozen]}`
+  } else {
+    if (dozen === 0) {
+      return units[unit]
+    }
+    dozen = `${dozens[dozen]}`
+    unit = `${units[unit]}`
+  }
+  return dozen + ' ' + unit
 }
